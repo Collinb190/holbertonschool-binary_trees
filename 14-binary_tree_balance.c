8b5a93b6fde264f8789c3663b579e46836923c2d
Collinb190 <collinb190@gmail.com>
2023-12-20 15:27:43 -0600

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (right_height - left_height);
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
+	return (left_height - right_height);
 }
