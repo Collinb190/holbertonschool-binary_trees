206862410d8ff8a090258c500f84cdadfa3a665f
Collinb190 <collinb190@gmail.com>
2023-12-19 23:11:10 -0600

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -25,7 +25,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 	if (parent->right != NULL)
 	{
-		new_node->right = parent->left;
+		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
 	parent->right = new_node;
